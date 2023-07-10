90233d8b9eb73add5f6d2b03faad6a9965a44653
mohit <mohityadavkkn24@gmail.com>
2023-07-10 14:28:47 +0530

    developer created

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,18 @@
 const router=require('express').Router();
+const mongoose= require('mongoose');
+const Developer=require('../modals/developer');
 
+// create developer
+
+router.post("/create",async (req,res)=>{
+    try{
+        const newdeveloper=await Developer.create(req.body);
+        res.status(201).json({message:"success"})
+    }
+    catch(err)
+    {
+        res.status(400).json({message:"error"})
+    }
+})
+
+module.exports=router;
