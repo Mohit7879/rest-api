3edb3e89e1db26a2ff7ee4b81d926c19fa406931
mohit <mohityadavkkn24@gmail.com>
2023-07-10 17:12:46 +0530

    crud operations created

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,61 @@ router.post("/create",async (req,res)=>{
     }
 })
 
+
+// get all developer
+
+router.get('/all',async(req,res)=>{
+    try{
+        const all=await Developer.find();
+        res.status(201).json({message:"success"})
+    }
+    catch(err){
+        res.status(400).json({message:"error"})
+    }
+})
+
+
+//  get only one developer
+
+router.get("/:id" , async(req,res)=>{
+    try{
+    const one=await Developer.findById(req.params.id);
+    res.status(201).json({message:"success"})
+    }
+    catch(err)
+    {
+        res.status(400).json({message:"error"})
+    }
+})
+
+// update a developer
+
+router.get("/update", async(req,res)=>{
+    try{
+
+        const{id:_id}= await req.params;
+        const{changes}= await req.body;
+        const updatedeveloper=await Developer.findByIdAndUpdate(...changes,_id);
+        res.status(201).json({message:"success"})
+    }
+    catch(err)
+    {
+        res.status(400).json({message:"error"})
+    }
+})
+
+router.get("/delete", async(req,res)=>{
+    try{
+
+        const{id:_id}= await req.params;
+      //  const{changes}= await req.body;
+        const deletedeveloper=await Developer.findByIdAndDelete(_id);
+        res.status(201).json({message:"success"})
+    }
+    catch(err)
+    {
+        res.status(400).json({message:"error"})
+    }
+})
+
 module.exports=router;
